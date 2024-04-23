9fbb96cc063904731fc0eeea8e1efd066d5b46a1
Robert Schofield <rschofield1992@gmail.com>
2024-04-23 13:30:45 -0400

    commit

---
+++ FizzWhiteBoard/FizzWhiteBoard/Program.cs
@@ -0,0 +1,27 @@
+﻿
+
+//when the number is divisible by 3 return the word fizz
+
+//when the number is divisible by 5 return the word buzz
+
+//when the number is divisible by both, return the word fizzbuzz
+
+
+
+namespace ChristmasWithTypes
+{
+    class Program
+    {
+        public static string Main(int number)
+        {  
+            if (number % 3 == 0)
+                return "fizz";
+            if (number % 5 == 0)
+                return "buzz";
+            else if (number % 3 == 0 && number % 5 == 0)
+                return "fizzbuzz";
+            else
+                return null;
+        }
+    }
+}
